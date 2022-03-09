4a98fb02ba7d0f02f19dd2936e5a663f6ff7cae5
alecs396 <a.swainston16@gmail.com>
2022-03-09 16:47:51 -0700

    Runs on 60 second intervals

--- main.py
+++ main.py
@@ -1,24 +1,49 @@
-from wsgiref import headers
+from pkgutil import extend_path
+from urllib import response
+from requests import Request, Session
+from requests.exceptions import ConnectionError, Timeout, TooManyRedirects
+import json
 import apikey
-import requests
+import time
 
-url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
-headers = {
-    'X-CMC_PRO_API_KEY' : apikey.key,
-    'Accepts' : 'application/json'
-}
+def getData(): 
+    url = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest'
 
-# Set Parameters of what crypto to track and what currency the price is displayed as.
-params = {
-    'start' : '1',
-    'limit' : '10',
-    'convert' : 'USD'
-}
+    # Set Parameters of what crypto to track and what currency the price is displayed as.
+    parameters = {
+        'start': '1',
+        'limit': '10',
+        'convert': 'USD'
+    }
 
-json = requests.get(url, params=params, headers=headers).json()
+    headers = {
+        'Accepts': 'application/json',
+        'X-CMC_PRO_API_KEY': apikey.key
+    }
 
-coins = json['data']
+    session = Session()
+    session.headers.update(headers)
+    
+    while True:
+        try:
+            response = session.get(url, params=parameters)
+            data = json.loads(response.text)
+            coins = data['data']
+            for x in coins:
+                print(x['symbol'], x['quote']['USD']['price'])
+            print("\n")
+        except (ConnectionError, Timeout, TooManyRedirects) as e:
+            print(e)
+        time.sleep(60)
+        
+    
+    # json = requests.get(url, params=params, headers=headers).json()
 
-for x in coins:
-    print(x['symbol'], x['quote']['USD']['price'])
+    # coins = json['data']
+    
+    # while True:
+    #     for x in coins:
+    #         print(x['symbol'], x['quote']['USD']['price'])
+        
+getData()
